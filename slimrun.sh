87388790814c51dd30d048aa204357ff52a62b2a
mtkirby <mkirby@jigen.mk.lan>
2016-07-03 22:36:11 -0500

    update

--- slimrun.sh
+++ slimrun.sh
@@ -2,16 +2,34 @@
 # 20151025 Kirby
 
 . /root/slimlib
+
+
+/root/slimshim.sh prep
+
 slimguess
     
 /root/slimshim.sh $ip $mac $routerip $routermac
-subnet=$(echo $ip |cut -d'.' -f1-3)
-route add -net ${subnet}.0/24 dev br-lan
-route add default gw $routerip
-echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 
-while :; do
-    sleep 10
-    # openwrt overwrites resolv.conf
-    grep 8.8.8.8 /etc/resolv.conf >/dev/null 2>&1 || echo 'nameserver 8.8.8.8' > /etc/resolv.conf
-done
+# Assume that we are on a /24 network
+# If you have problems connecting to other IPs in a nearby subnet, try tuning the cidr
+ipnetwork="${ip%.*}.0/24"
+route add -net $ipnetwork dev br-lan >/dev/null 2>&1
+ripnetwork="${routerip%.*}.0/24"
+route add -net $ripnetwork dev br-lan >/dev/null 2>&1
+route add default gw $routerip >/dev/null 2>&1
+
+# set
+echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+/etc/init.d/dnsmasq stop >/dev/null 2>&1
+sniff=$(tcpdump -c1 -nni br-lan "not ip6 and udp and port 53" 2>/dev/null)
+# 04:40:30.876500 IP 192.168.1.19.59369 > 192.168.1.1.53: 13937+ A? cnn.com. (25)
+dns=$(echo $sniff |awk '{print $5}' |cut -d'.' -f1-4)
+grep -q $dns /etc/resolv.conf
+if [ $? != 0 ]; then
+    echo "nameserver $dns" > /etc/resolv.conf
+fi
+grep -q 8.8.8.8 /etc/resolv.conf
+if [ $? != 0 ]; then
+    echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+fi
+
